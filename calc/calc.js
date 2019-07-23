01cbbb74a480f9b205cb53a79736276deb21a191
James Okunlade <james.okunlade@gmail.com>
2019-07-23 14:37:24 +0100

    calc.js added

---
+++ calc/calc.js
@@ -0,0 +1,17 @@
+const calc = {
+  add: (a, b) => {
+    return a + b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  }
+}
+
+
+module.exports = calc;
