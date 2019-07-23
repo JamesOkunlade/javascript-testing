6e27a94b8a49a93feb321b93601c8cc4af418a32
Adnan Afsari <aarahul1991@gmail.com>
2019-07-23 19:51:45 +0600

    ceaser test added

---
+++ ceasar/ceasar.test.js
@@ -0,0 +1,13 @@
+const ceasar = require('./ceasar');
+​
+test('converts a single word', () => {
+    expect(ceasar('hello')).toBe('khoor');
+});
+​
+test('converts a sentence', () => {
+    expect(ceasar('hello world')).toBe('khoor zruog');
+});
+​
+test('converts a sentence with punctuation', () => {
+    expect(ceasar('hello, world')).toBe('khoor, zruog');
+});
